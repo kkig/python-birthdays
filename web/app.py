4b3bc3cb145e7f90a445fbcbb4d3fc8a23a7a104
kkig <kigoshi.kanazawa@gmail.com>
2024-11-09 18:10:32 +0900

    Refactor DB class

--- web/app.py
+++ web/app.py
@@ -22,22 +22,21 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
         bday = request.form.get("bday")
         birthday = date.fromisoformat(bday)
         if name and bday:
             return render_template("index.html", name=name, bday=birthday)
-        
+
         # Invalid input
         return render_template("index.html", error="Invalid input")
 
     else:
-
         # TODO: Display the entries in the database on index.html
 
         return render_template("index.html", name=request.form.get("name"))
@@ -45,12 +44,13 @@ def index():
 
 conn = None
 
+
 @app.route("/names")
 def listNames():
     global conn
     # conn = None
     if not conn:
-        conn = DB(password_file="/run/secrets/db-password")
+        conn = DB()
         conn.populate()
     rec = conn.query_names()
 
@@ -58,6 +58,7 @@ def listNames():
     for c in rec:
         respose = respose + "<div> Hello " + c + "</div>"
     return respose
+
 
 if __name__ == "__main__":
     app.run()
--- web/dbloader.py
+++ web/dbloader.py
@@ -7,22 +7,32 @@ from mysql.connector import errorcode
 # https://dev.mysql.com/doc/connector-python/en/connector-python-introduction.html
 
 
-class DB:
-    def __init__(self, database="example", host="db", user="root", password_file=None):
-        try:
-            pwf = open(password_file, "r")
-            self.connection = mysql.connector.connect(
-                user=user,
-                password=pwf.read(),
-                host=host, # name of the mysql service as set in the docker compose file
-                database=database,
-                auth_plugin="mysql_native_password"
-            )
-            pwf.close()
-            self.cursor = self.connection.cursor()
+class Config:
+    @staticmethod
+    def init():
+        _config = {
+            "user": "root",
+            "host": "db",
+            "database": "example",
+        }
 
-            # self.connection = self._connect(password_file)
-            # self.cursor = self.connection.cursor()
+        try:
+            pwd = open("/run/secrets/db-password", "r")
+            _config["password"] = pwd.read()
+            pwd.close()
+            return _config
+        except ValueError:
+            print("Error opening password file.")
+
+
+class DB:
+    def __init__(self):
+        self.conn = self._start(Config.init())
+        self.cursor = self.conn.cursor()
+
+    def _start(self, config):
+        try:
+            return mysql.connector.connect(**config)
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Something is wrong with your user name or password")
@@ -30,30 +40,24 @@ class DB:
                 print("Database does not exist")
             else:
                 print(err)
-    
+            exit(1)
 
     def populate(self):
         tableName = "persons"
-        # datas = [
-        #     {
-        #     "name": "name", 
-        #     "type": "VARCHAR(255))"
-        #     },
-        #     {
-        #         "name": "birthday",
-        #         "type": ""
-        #      }
-        # ]
+
         # self.cursor.execute("DROP TABLE IF EXISTS persons")
         # self.cursor.execute("CREATE TABLE persons (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), birthday DATE)")
         try:
-            self.cursor.execute(f"CREATE TABLE {tableName} (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))")
-            self.cursor.executemany(f"INSERT INTO {tableName} (id, name) VALUES (%s, %s);", [(i, "User #%d"% i) for i in range(5)])
+            self.cursor.execute(
+                f"CREATE TABLE {tableName} (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))"
+            )
+            self.cursor.executemany(
+                f"INSERT INTO {tableName} (id, name) VALUES (%s, %s);",
+                [(i, "User #%d" % i) for i in range(5)],
+            )
             self.connection.commit()
         except:
             print("Error creating table persons.")
-
-
 
     def query_names(self):
         self.cursor.execute("SELECT name FROM persons")
